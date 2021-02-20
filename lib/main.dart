855ce661e842015cc93f3c95ec9e392a22822122
Kristos Akalugwu <kristosakalugwu@gmail.com>
2021-02-20 03:28:01 +0100

    Final Upload

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:battery_app/pages/battery_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Battery Level by Kristos';
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          primarySwatch: Colors.blue,
+        ),
+        home: BatteryPage(),
+      );
+}
